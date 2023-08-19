489ef34223fd2f7a1608c7fe4d53b796752ac15b
Kycool13 <mattingly.luke@yahoo.com>
2023-08-19 17:46:29 -0600

    locally running server up

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const tyeDefs = `#graphql
-    type = Query{
+import {ApolloServer} from '@apollo/server';
+import {startStandaloneServer} from '@apollo/server/standalone';
+
+const typeDefs = `#graphql
+    type Query {
         greeting: String
     }
 `;
@@ -9,3 +12,11 @@ const resolvers = {
         greeting: () => 'Hello World',
     }
 }
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+startStandaloneServer(server, {
+    listen: { port: 9000 },
+}).then(({ url }) => {
+    console.log(`Server is running at ${url}`);
+});
